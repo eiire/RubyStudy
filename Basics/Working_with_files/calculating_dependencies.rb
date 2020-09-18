b8d3500902c60252d8fb2fc949faae4cf8f7d85a
Dmitriy <bulax.d.2101@gmail.com>
2020-09-18 21:07:59 +0700

    fix Min, max and refactor code

--- Basics/Working_with_files/calculating_dependencies.rb
+++ Basics/Working_with_files/calculating_dependencies.rb
@@ -2,74 +2,125 @@
 
 require 'csv'
 
-# @param [Array] matrix_data
-# @param [Integer] n_rows
-# @param [Integer] n_columns
-# @return [Array]
-def find_mean(matrix_data, n_rows, n_columns)
-  mean = []
-  (0..(n_columns - 1)).each do |_|
-    mean.append(0)
-  end
-
-  matrix_data.each do |row|
-    (0..row.length - 1).each do |i|
-      mean[i] += row[i].to_f
-    end
-  end
-
-  (0..(n_columns - 1)).each do |i|
-    mean[i] /= n_rows
-  end
-
-  mean
-end
-
-# @param [Array] matrix_data
-# @param [Array] mean
-# @param [Integer] n_columns
-# @param [Integer] n_rows
-# @return [Array]
-def variance(matrix_data, mean, n_columns, n_rows)
-  matrix_data.each do |row|
-    (0..row.length - 1).each do |i|
-      row[i] = row[i].to_f - mean[i].to_f # центрируем
-      row[i] *= row[i]
-    end
-  end
-
-  res = []
-  (0..(n_columns - 1)).each do |_|
-    res.append(0)
-  end
-
-  matrix_data.each do |row|
-    (0..row.length - 1).each do |i|
-      res[i] += row[i]
-    end
-  end
-
-  (0..(n_columns - 1)).each do |i|
-    res[i] /= n_rows - 1
-    res[i] = res[i].round(4)
-  end
-
-  res
-end
-
-# data preparation
-n_columns = CSV.read('demo32.csv').shift.length
-n_rows = CSV.read('demo32.csv').length
-data_matrix = CSV.read('demo32.csv').each do |row|
-  (0..n_columns - 1).each do |i|
-    row[i] = row[i].split(',').join('.')
+class String
+  def numeric?
+    Float(self) != nil rescue false
   end
 end
 
-print 'max - ', CSV.read('demo32.csv').max
-puts ';'
-print 'min - ', CSV.read('demo32.csv').min
-puts ';'
-print 'mean - ', find_mean(data_matrix, n_rows, n_columns)
-puts ';'
-print 'variance - ', variance(data_matrix, find_mean(data_matrix, n_rows, n_columns), n_columns, n_rows)
+class EconomCalculator
+  def initialize(file_name)
+    @is_data = true
+    @file_name = file_name
+    @n_columns = CSV.read(file_name)[0].length - 1
+    @n_rows = CSV.read(file_name).length - 1
+    @data_matrix = CSV.read(file_name)
+    @columns_name = @data_matrix[0..0][0]
+    @data_matrix = @data_matrix[1..@n_rows]
+
+    @data_matrix = @data_matrix.each do |row|
+      (0..@n_columns).each do |i|
+        row[i] = row[i].split(',').join('.')
+        if row[i].numeric?
+          row[i] = row[i].to_f
+        else
+          @is_data = false
+        end
+      end
+    end
+  end
+
+  def columns_name
+    return 'Incorrect data!' + "\n" unless @is_data
+
+    @columns_name.join(',') + "\n"
+  end
+
+  def max
+    return 'Incorrect data!' + "\n" unless @is_data
+
+    res = []
+    (0..@n_columns).each do |i|
+      vals_columns = []
+      @data_matrix.each do |row|
+        vals_columns.append row[i]
+      end
+      res.append vals_columns.max
+    end
+
+    res.join(',') + "\n"
+  end
+
+  def min
+    return 'Incorrect data!' + "\n" unless @is_data
+
+    res = []
+    (0..@n_columns).each do |i|
+      vals_columns = []
+      @data_matrix.each do |row|
+        vals_columns.append row[i]
+      end
+      res.append vals_columns.min
+    end
+
+    res.join(',') + "\n"
+  end
+
+  def find_mean
+    return 'Incorrect data!' + "\n" unless @is_data
+
+    mean = []
+    (0..(@n_columns)).each do |_|
+      mean.append(0)
+    end
+
+    @data_matrix.each do |row|
+      (0..row.length - 1).each do |i|
+        mean[i] += row[i]
+      end
+    end
+
+    (0..@n_columns).each do |i|
+      mean[i] /= @n_rows
+    end
+
+    mean.join(',') + "\n"
+  end
+
+  def variance
+    return 'Incorrect data!' + "\n" unless @is_data
+
+    mean = find_mean.split(',')
+    @data_matrix.each do |row|
+      (0..row.length - 1).each do |i|
+        row[i] = row[i] - mean[i].to_f # центрируем
+        row[i] *= row[i]
+      end
+    end
+
+    res = []
+    (0..@n_columns).each do |_|
+      res.append(0)
+    end
+
+    @data_matrix.each do |row|
+      (0..@n_columns).each do |i|
+        res[i] += row[i]
+      end
+    end
+
+    (0..@n_columns).each do |i|
+      res[i] /= @n_rows - 1
+      res[i] = res[i].round(4)
+    end
+
+    res.join(',') + "\n"
+  end
+end
+
+data = EconomCalculator.new('demo32.csv')
+print data.columns_name
+print data.min
+print data.max
+print data.find_mean
+print data.variance
