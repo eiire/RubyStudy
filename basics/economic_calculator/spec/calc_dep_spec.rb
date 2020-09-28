1fc8e247198f34564b9a8b4d4556ccae5e83b684
Dmitriy <bulax.d.2101@gmail.com>
2020-09-28 14:04:27 +0700

    refactor tests for economic_calculator

--- basics/economic_calculator/spec/calc_dep_spec.rb
+++ basics/economic_calculator/spec/calc_dep_spec.rb
@@ -2,31 +2,33 @@ require 'rspec'
 require 'csv'
 require_relative '../lib/econom_calculator'
 
-describe 'EconomCalculator' do
-  it 'min' do
-    data = EconomCalculator.new('spec/demo32.csv')
-    test_list = [49_378, 184_398, 396_589, 580_720, 559_918, 665_904]
-    expect(data.min.split(',')[1]).to eq test_list.min.to_f.to_s
-  end
+RSpec.describe EconomCalculator do
+  describe 'converter' do
+    let(:calculator) { EconomCalculator.new('spec/demo32.csv') }
+    let(:incorrect_calculator) { EconomCalculator.new('spec/demo32_incorrect.csv') }
+    let(:test_list) { [49_378, 184_398, 396_589, 580_720, 559_918, 665_904] }
 
-  it 'max' do
-    data = EconomCalculator.new('spec/demo32.csv')
-    test_list = [49_378, 184_398, 396_589, 580_720, 559_918, 665_904]
-    expect(data.max.split(',')[1]).to eq test_list.max.to_f.to_s
-  end
+    context 'Find min' do
+      it { expect(calculator.min.split(',')[1]).to eq test_list.min.to_f.to_s }
+    end
 
-  it 'mean Floating point processing recorded as 6,7 and 6.7' do
-    expect(EconomCalculator.new('spec/demo32.csv').find_mean.split(',')[2].to_f.round(2)).to eq 2.92
-  end
+    context 'Find max' do
+      it { expect(calculator.max.split(',')[1]).to eq test_list.max.to_f.to_s }
+    end
 
-  it 'variance Floating point processing recorded as 6,7 and 6.7' do
-    expect(EconomCalculator.new('spec/demo32.csv').variance.split(',')[2].to_f.round(2)).to eq 2.56
-  end
+    context 'Find mean with float point processing recorded as 6,7 and 6.7' do
+      it { expect(calculator.find_mean.split(',')[2].to_f.round(2)).to eq 2.92 }
+    end
 
-  it 'mean max min variance with incorrect data' do
-    expect(EconomCalculator.new('spec/demo32_incorrect.csv').find_mean).to eq "Incorrect data!\n"
-    expect(EconomCalculator.new('spec/demo32_incorrect.csv').max).to eq "Incorrect data!\n"
-    expect(EconomCalculator.new('spec/demo32_incorrect.csv').min).to eq "Incorrect data!\n"
-    expect(EconomCalculator.new('spec/demo32_incorrect.csv').variance).to eq "Incorrect data!\n"
+    context 'Find variance with float point processing recorded as 6,7 and 6.7' do
+      it { expect(calculator.variance.split(',')[2].to_f.round(2)).to eq 2.56 }
+    end
+
+    context 'Find mean max min variance with incorrect data' do
+      it { expect(incorrect_calculator.find_mean).to eq "Incorrect data!\n" }
+      it { expect(incorrect_calculator.max).to eq "Incorrect data!\n" }
+      it { expect(incorrect_calculator.min).to eq "Incorrect data!\n" }
+      it { expect(incorrect_calculator.variance).to eq "Incorrect data!\n" }
+    end
   end
 end
